b7c46cfd802d27c2a1c49cdeb52a71cbd322d25d
Haiito92 <116373874+Haiito92@users.noreply.github.com>
2025-10-26 18:16:41 +0100

    Start Bot

---
+++ main.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+#Env variables
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+#Intents
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+#Create my bot commands
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Bot {bot.user} has connected to Discord!")
+
+bot.run(TOKEN)
